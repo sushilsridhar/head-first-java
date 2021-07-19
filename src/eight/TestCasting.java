de9edebf08991e7927b79486522342b178d85b7a
sushil <sushilsridhar16@gmail.com>
2021-07-19 21:10:37 +0530

    chapter eight

---
+++ src/eight/TestCasting.java
@@ -0,0 +1,24 @@
+package eight;
+
+/*
+ *  OBJECT TYPE REFERENCE VARIABLE CAN BE CASTED TO TYPE OF THE ACTUAL OBJECT STORED IN HEAP
+ */
+
+public class TestCasting  {
+    public static void main(String[] args)
+    {
+
+        TestCasting m = new TestCasting();
+
+        System.out.println(m.getData(m));
+
+
+        TestCasting m1 = (TestCasting) m.getData(m); // this will work as casting is done
+    }
+
+    public Object getData(Object o)
+    {
+        System.out.println("okai :P");
+        return o;
+    }
+}
