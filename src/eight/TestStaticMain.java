5b9292ec1cfc7a03eb72f9d5dbedb24ade9a21bd
Sushil <sushilsridhar16@gmail.com>
2021-08-09 20:55:52 +0530

    reviewed chapter eight of book

---
+++ src/eight/TestStaticMain.java
@@ -0,0 +1,34 @@
+package eight;
+
+/*
+ *   Accessing static method using reference variable --> WILL WORK BUT SHOULDN'T BE DONE
+ */
+
+class TestStaticHelper
+{
+    public static void show()
+    {
+        System.out.println("Hello World show :D");
+    }
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello World :D");
+
+    }
+
+}
+
+public class TestStaticMain
+{
+    public static void main(String[] args)
+    {
+        String s[] ={};
+
+        TestStaticHelper c = new TestStaticHelper();
+        c.main(s);
+        c.show();
+
+    }
+}
