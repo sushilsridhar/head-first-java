ff5dd5d5474f76b2f994226080cd8a9322d5eabe
Sushil <sushilsridhar16@gmail.com>
2021-09-01 21:08:51 +0530

    Abstract class example

---
+++ src/eight/AbstractExample.java
@@ -0,0 +1,20 @@
+package eight;
+
+/*
+    Number is the abstract class, Integer, Double and Float class extends the class Number
+ */
+public class AbstractExample {
+
+    public void printNumber(Number i) {
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+
+        AbstractExample abstractExample = new AbstractExample();
+
+        abstractExample.printNumber(6);
+        abstractExample.printNumber(5.5);
+        abstractExample.printNumber(5f);
+    }
+}
---
+++ src/eight/Car.java
@@ -0,0 +1,40 @@
+package eight;
+
+abstract class Fuel {
+    abstract void printFuelType();
+}
+class Petrol extends Fuel {
+    public void printFuelType() {
+        System.out.print("petrol");
+    }
+}
+class Diesel extends Fuel {
+    public void printFuelType() {
+        System.out.print("diesel");
+    }
+}
+
+abstract class Vehicle {
+
+    public void fuel(Fuel fuel) {
+        System.out.print("fuel Vehicle type - ");
+        fuel.printFuelType();
+    }
+
+    abstract void serviceEngine();
+}
+
+public class Car extends Vehicle {
+
+    @Override
+    void serviceEngine() {
+        System.out.println("Service Car engine");
+    }
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.fuel(new Petrol());
+        car.fuel(new Diesel());
+    }
+}
